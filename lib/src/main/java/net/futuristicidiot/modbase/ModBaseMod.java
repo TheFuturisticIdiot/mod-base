fcf92d98e018ebefe7e070de0581be9cde48ba92
TheFuturisticIdiot <dannyeuan09@gmail.com>
2026-03-12 11:09:14 +0000

    Probably fixed dependency issue?

---
+++ lib/src/main/java/net/futuristicidiot/modbase/ModBaseMod.java
@@ -0,0 +1,13 @@
+package net.futuristicidiot.modbase;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Dummy mod entry point so Forge's mod loader recognises modbase as a loaded mod.
+ * All actual initialisation is done by user mods extending ModBase.
+ */
+@Mod("modbase")
+public class ModBaseMod {
+    public ModBaseMod() {
+        // Hi :3
+    }
+}
